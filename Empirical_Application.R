cbb821da41a0e7c2c9bd103eacb39626d3217064
ViniciusBardelin <125508443+ViniciusBardelin@users.noreply.github.com>
2026-02-06 22:31:44 -0300

    Update Empirical_Application.R

--- Empirical_Application.R
+++ Empirical_Application.R
@@ -67,8 +67,7 @@ for (i in 1:n_oos) {
         
         if (anyNA(r)) {
           idx <- is.na(r)
-          r[idx] <- returns_c[idx, j]   # conservative option
-          # r[idx] <- 0                 # more “neutral” option
+          r[idx] <- returns_c[idx, j]   
         }
         
         if (all(is.finite(r)) && sd(r) > 1e-10) {
@@ -154,20 +153,22 @@ df_oos <- data.frame(
 write.csv(df_oos, "oos_data.csv", row.names = FALSE)
 
 # Check VaR 1%
-sum(df_oos$Return < df_oos$VaR_GARCH_1)/2846 # [1] 0.01124385
+sum(df_oos$Return < df_oos$VaR_GARCH_1)/2846 # 0.01124385
 
-sum(df_oos$Return < df_oos$VaR_MSGARCH_1)/2846 # [1] 0.01089248
+sum(df_oos$Return < df_oos$VaR_MSGARCH_1)/2846 # 0.01089248
 
-sum(df_oos$Return < df_oos$VaR_GAS_1)/2846 # [1] 0.00983837
+sum(df_oos$Return < df_oos$VaR_GAS_1)/2846 # 0.00983837
 
 # Check VaR 5%
-sum(df_oos$Return < df_oos$VaR_GARCH_5)/2846 # [1] 0.05165144
+sum(df_oos$Return < df_oos$VaR_GARCH_5)/2846 # 0.05165144
 
-sum(df_oos$Return < df_oos$VaR_MSGARCH_5)/2846 # [1] 0.05411103
+sum(df_oos$Return < df_oos$VaR_MSGARCH_5)/2846 # 0.05411103
 
-sum(df_oos$Return < df_oos$VaR_GAS_5)/2846 # [1] 0.04884048
+sum(df_oos$Return < df_oos$VaR_GAS_5)/2846 # 0.04884048
 
-# HAR
+###########
+#   HAR   #
+###########
 
 # InS
 RV <- as.xts(df$RV_APPLE, order.by = df$DATE)
@@ -175,10 +176,10 @@ RV <- as.xts(df$RV_APPLE, order.by = df$DATE)
 RV_ins <- RV[1:2500]
 
 sigmaHAR_completo <- matrix(NA_real_, nrow = n_tot, ncol = 1,
-                          dimnames = list(NULL, c("HAR")))
+                            dimnames = list(NULL, c("HAR")))
 
 fit_HAR <- HARmodel(RV_ins, periods = c(1,5,22), RVest = c("rCov"), 
-         type = "HAR", h = 1, transform = NULL, inputType = "RM")
+                    type = "HAR", h = 1, transform = NULL, inputType = "RM")
 
 sigmaHAR_completo[23:n_ins, "HAR"] <- fit_HAR$fitted.values
 
@@ -187,25 +188,79 @@ ES_1 <- ES_5 <- VaR_1 <- VaR_5 <- sigmaHAR <- matrix(0, ncol = 1, nrow = n_oos, 
 r_oos <- c()
 for (i in 1:n_oos) {
   print(i)
+  
+  returns_window <- returns[i:(i + n_ins - 1)]
+  mu <- mean(returns_window)
+  
+  returns_c <- as.numeric(returns_window - mu)
+  
   rv_window <- as.xts(RV[i:(i + n_ins - 1)])
-
-  fit_HAR <- HARmodel(rv_window, periods = c(1,5,22), RVest = c("rCov"), 
-                type = "HAR", h = 1, transform = NULL, inputType = "RM")
+  
+  fit_HAR <- HARmodel(
+    rv_window,
+    periods = c(1, 5, 22),
+    RVest = c("rCov"),
+    type = "HAR",
+    h = 1,
+    transform = NULL,
+    inputType = "RM"
+  )
   
   sigmaHAR[i, "HAR"] <- predict(fit_HAR)
   
-  #sigmaHAR_completo[i + n_ins, "HAR"] <- fit_HAR$fitted.values
   sigmaHAR_completo[i + n_ins, "HAR"] <- as.numeric(tail(fit_HAR$fitted.values, 1))
   
-  res_HAR <- as.numeric(tail(fit_HAR$residuals, 1))
-
-  VaR_1[i, "HAR"] = mu + sqrt(sigmaHAR[i, "HAR"]) * quantile(res_HAR, 0.01)
-  ES_1[i, "HAR"] <- mean(returns_window[returns_window < VaR_1[i, "HAR"]])
-
-  # tirar sqrt?
-  VaR_5[i, "HAR"] = mu + sqrt(sigmaHAR[i, "HAR"]) * quantile(res_HAR, 0.05)
-  ES_5[i, "HAR"] <- mean(returns_window[returns_window < VaR_5[i, "HAR"]])
+  # --------- resíduos padronizados corretos (na escala de volatilidade) ----------
+  rv_hat_is <- as.numeric(na.omit(fit_HAR$fitted.values))  # VARIÂNCIA ajustada; tamanho n_ins-22
+  r_c_is    <- returns_c[23:n_ins]                         # alinha com os fitted (remove 22 primeiros)
+  
+  eps <- 1e-12
+  sigma_hat_is <- sqrt(pmax(rv_hat_is, eps))               # VOLATILIDADE ajustada (desvio padrão)
+  
+  res_HAR <- r_c_is / sigma_hat_is                         # z_t padronizado
+  
+  q01 <- quantile(res_HAR, 0.01, na.rm = TRUE)
+  q05 <- quantile(res_HAR, 0.05, na.rm = TRUE)
+  
+  
+  sigma_oos <- sqrt(as.numeric(sigmaHAR[i, "HAR"]))        
+  
+  VaR_1[i, "HAR"] <- mu + sigma_oos * q01
+  ES_1[i, "HAR"]  <- mean(returns_window[returns_window < VaR_1[i, "HAR"]], na.rm = TRUE)
+  
+  VaR_5[i, "HAR"] <- mu + sigma_oos * q05
+  ES_5[i, "HAR"]  <- mean(returns_window[returns_window < VaR_5[i, "HAR"]], na.rm = TRUE)
   
   r_oos[i] <- returns[i + n_ins]
-  
 }
+
+# InS
+df_sigmaHAR_completo <- data.frame(
+  Date = df$DATE,
+  Returns = df$RETURNS_APPLE,
+  Sigma2_GARCH = sigmaHAR_completo[, "HAR"],
+  RV_APPLE = df$RV_APPLE
+)
+
+write.csv(df_sigmaHAR_completo, "ins_HAR_data.csv", row.names = FALSE)
+
+# OoS
+df_oos_HAR <- data.frame(
+  Date = df$DATE[(n_ins + 1):n_tot],
+  Return = r_oos,
+  Vol_HAR = sqrt(sigmaHAR[, "HAR"]),
+  
+  VaR_HAR_1 = VaR_1[, "HAR"],
+  ES_HAR_1 = ES_1[, "HAR"],
+  
+  VaR_HAR_5 = VaR_5[, "HAR"],
+  ES_HAR_5 = ES_5[, "HAR"],
+  
+  RV_APPLE = df$RV_APPLE[(n_ins + 1):n_tot]
+)
+
+write.csv(df_oos_HAR, "oos_HAR_data.csv", row.names = FALSE)
+
+# Check VaR RES3
+sum(df_oos_HAR$Return < df_oos_HAR$VaR_HAR_1)/2846 # 0.01405481
+sum(df_oos_HAR$Return < df_oos_HAR$VaR_HAR_5)/2846 # 0.05551651
